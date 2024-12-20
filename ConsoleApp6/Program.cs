cc550b1c67cc1fd3b94de7a723cee63047191572
abdullozoda_oo8 <abdullozodaanush8@gmail.com>
2024-12-20 17:03:23 +0500

    first commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Animal
+    {
+        public string kind="собака";
+        public string name="фуркат";
+        public int age=6;
+        public int weight = 90;
+        
+        
+        public void PrintInfo()
+        {
+            Console.WriteLine("тип: "+kind);
+            Console.WriteLine("имя: "+name);
+            Console.WriteLine("лет: "+age+" years");
+            Console.WriteLine("вес: "+weight+"kg");
+    
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+         
+            animal.PrintInfo();
+        }
+    }
+}
